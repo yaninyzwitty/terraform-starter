4b86b385c064bf6b3186c7152b6fbd713a86b1e6
yaninyzwitty <ianmwa143@gmail.com>
2025-08-27 20:19:36 +0300

    initial commit

---
+++ ecs.tf
@@ -0,0 +1,101 @@
+# ----------------
+# ECS Cluster
+# ----------------
+resource "aws_ecs_cluster" "main" {
+  name = "ecs-cluster"
+}
+
+# ----------------
+# IAM Role for ECS tasks
+# ----------------
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ----------------
+# ECS Task Definition
+# ----------------
+resource "aws_ecs_task_definition" "app" {
+  family                   = "my-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "my-app"
+      image     = "nginx:latest"
+      essential = true
+      portMappings = [{
+        containerPort = 80
+        hostPort      = 80
+      }]
+    }
+  ])
+}
+
+# ----------------
+# ECS Service
+# ----------------
+resource "aws_ecs_service" "app" {
+  name            = "my-app-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id # ✅ use all created public subnets
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_tasks.id]
+  }
+}
+
+# ----------------
+# Security Group
+# ----------------
+resource "aws_security_group" "ecs_tasks" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-tasks-sg"
+  }
+}
+
+
+
+# 4577932
+
+
